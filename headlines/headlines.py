1c3aeacf08754a79666f39ee4c66c89074184a09
Abstract Thinking <abstract.thinking@w0lk37.de>
2016-06-19 19:19:07 +0000

    Fixing url

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-BBC_FEEDS = {'bbc': 'http://feeds.bbci.co.ui/news/rss.xml',
+BBC_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 	     'cnn': 'http://rss.cnn.com/rss/edition.rss',
 	     'fox': 'http://feeds.foxnews.com/foxnews/latest',
 	     'iol': 'http://www.iol.co.za/cmlink/1.640' }
