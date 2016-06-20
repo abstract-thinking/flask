f33ee02cf196e46a53fd32a31e7297af8241c07f
Abstract Thinking <abstract.thinking@w0lk37.de>
2016-06-20 18:28:27 +0000

    Using for loop in template and object.

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -29,12 +29,7 @@ def iol():
 
 def get_news(publication):
 	feed = feedparser.parse(BBC_FEEDS[publication])
-	first_article = feed['entries'][0]
-
-	return render_template("home.html",
-		title=first_article.get("title"),
-		published=first_article.get("published"),
-		summary=first_article.get("summary"))
+	return render_template("home.html", articles=feed['entries'])
 
 if __name__ == '__main__':
 	app.run(port=5000, debug=True)
