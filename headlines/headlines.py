3f38bfaa3a395bdbfcbb7f0e98db2055e674175f
Abstract Thinking <abstract.thinking@w0lk37.de>
2016-06-20 18:05:31 +0000

    Separat html from python code.

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,6 +1,7 @@
 import feedparser
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -30,15 +31,10 @@ def get_news(publication):
 	feed = feedparser.parse(BBC_FEEDS[publication])
 	first_article = feed['entries'][0]
 
-	return """<html>
-		<body>
-		<h1> Headlines </h1>
-		<b>{0}</b><br/>
-		<i>{1}</i><br/>
-		<p>{2}</p><br/>
-		</body>
-		</html>""".format(first_article.get("title"),
-		first_article.get("published"), first_article.get("summary"))
+	return render_template("home.html",
+		title=first_article.get("title"),
+		published=first_article.get("published"),
+		summary=first_article.get("summary"))
 
 if __name__ == '__main__':
 	app.run(port=5000, debug=True)
