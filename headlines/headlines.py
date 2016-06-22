1ad8a40017893b7745e395a3df121e851ac115e2
Abstract Thinking <abstract.thinking@w0lk37.de>
2016-06-22 21:03:15 +0000

    Added temperature functionality

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,4 +1,7 @@
 import feedparser
+import json
+import urllib2
+import urllib
 
 from flask import Flask
 from flask import render_template
@@ -11,35 +14,48 @@ BBC_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 	     'fox': 'http://feeds.foxnews.com/foxnews/latest',
 	     'iol': 'http://www.iol.co.za/cmlink/1.640' }
 
+DEFAULTS = {'publication':'bbc',
+	    'city':'London,UK' }
+
+WHEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather={}&units=metric&appid={}'
+
 @app.route("/")
-def get_news():
-	query = request.args.get("publication")
+def home():
+	publication = request.args.get('publication')
+	if not publication:
+		publication = DEFAULTS['publication']
+	articles = get_news(publication)
+
+	with open('weather.txt', r+) as f:
+		weather_api_key=f.read().splitlines()
+	city = request.args.get('city')
+	if not city:
+		city = DEFAULTS['city']
+	weather = get_weather(city, weather_api_key)
+	return render_template("home.html",
+		articles=feed['entries'],
+		weather=weather)
+
+def get_news(query):
 	if not query or query.lower() not in RSS_FEEDS:
-		publication = "bbc"
+		publication = DEFAULTS['publication']
 	else:
 		publication = query.lower()
-	feed = feedparser.parse(RSS_FEEDS[publication])
-	return render_template("home.html", articles=feed['entries'])
-
-@app.route("/bbc")
-def bbc():
-	return get_news('bbc')
-
-@app.route("/cnn")
-def cnn():
-	return get_news('cnn')
-
-@app.route("/fox")
-def fox():
-	return get_news('fox')
-
-@app.route("/iol")
-def iol():
-	return get_news('iol') 
-
-def get_news(publication):
 	feed = feedparser.parse(BBC_FEEDS[publication])
-	return render_template("home.html", articles=feed['entries'])
+	return feed['entries']
+
+def get_weather(query, weather_api_key): 
+	query = urllib.quote(query)
+	url = WHEATHER_URL.format(query, weather_api_key)
+	data = urllib2.urlopen(url).read()
+	parsed = json.loads(data)
+	weather = None
+	if parsed.get("weather"):
+		weather = {"description" : parsed["weather"][0]["description"],
+			"temperature" : parsed["main"]["temp"],
+			"city" : parsed["name"],
+			"country" : parsed["sys"]["country"]}
+	return weather
 
 if __name__ == '__main__':
 	app.run(port=5000, debug=True)
