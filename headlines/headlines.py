5552cb52751a18924a9a2b8eca46647d11c3e752
Abstract Thinking <abstract.thinking@w0lk37.de>
2016-06-24 17:00:38 +0000

    Bugfixing.

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,7 +1,10 @@
 import feedparser
 import json
+import os
 import urllib2
 import urllib
+
+import logging
 
 from flask import Flask
 from flask import render_template
@@ -9,7 +12,7 @@ from flask import request
 
 app = Flask(__name__)
 
-BBC_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 	     'cnn': 'http://rss.cnn.com/rss/edition.rss',
 	     'fox': 'http://feeds.foxnews.com/foxnews/latest',
 	     'iol': 'http://www.iol.co.za/cmlink/1.640' }
@@ -17,7 +20,7 @@ BBC_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 DEFAULTS = {'publication':'bbc',
 	    'city':'London,UK' }
 
-WHEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather={}&units=metric&appid={}'
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}"
 
 @app.route("/")
 def home():
@@ -26,14 +29,19 @@ def home():
 		publication = DEFAULTS['publication']
 	articles = get_news(publication)
 
-	with open('weather.txt', r+) as f:
-		weather_api_key=f.read().splitlines()
+	script_dir = os.path.dirname(__file__)
+	abs_file_path = os.path.join(script_dir, "weather.txt")
+	
+	f = open(abs_file_path, 'r')
+	weather_api_key = f.readline()
+	f.close()
+	
 	city = request.args.get('city')
 	if not city:
 		city = DEFAULTS['city']
-	weather = get_weather(city, weather_api_key)
+	weather = get_weather(city, weather_api_key.strip())
 	return render_template("home.html",
-		articles=feed['entries'],
+		articles=articles,
 		weather=weather)
 
 def get_news(query):
@@ -41,12 +49,13 @@ def get_news(query):
 		publication = DEFAULTS['publication']
 	else:
 		publication = query.lower()
-	feed = feedparser.parse(BBC_FEEDS[publication])
+	feed = feedparser.parse(RSS_FEEDS[publication])
 	return feed['entries']
 
 def get_weather(query, weather_api_key): 
 	query = urllib.quote(query)
-	url = WHEATHER_URL.format(query, weather_api_key)
+	url = WEATHER_URL.format(query, weather_api_key)
+	app.logger.info(url)
 	data = urllib2.urlopen(url).read()
 	parsed = json.loads(data)
 	weather = None
