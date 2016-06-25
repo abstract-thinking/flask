39286bfcfebc964e43c9bc1fcb7e3433cad4330a
Abstract Thinking <abstract.thinking@w0lk37.de>
2016-06-25 17:52:39 +0000

    Fixed bugs.

--- crimemap/db_setup.py
+++ crimemap/db_setup.py
@@ -9,14 +9,15 @@ try:
 	with connection.cursor() as cursor:
 		sql = "CREATE DATABASE IF NOT EXISTS crimemap"
 		cursor.execute(sql)
-		sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
+		sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes(
 			id INT NOT NULL AUTO_INCREMENT,
 			latitude FLOAT(10,6),
 			longitude FLOAT(10,6),
 			date DATETIME,
-			category VARCHAR(1000),
+			category VARCHAR(50),
+			description VARCHAR(1000),
 			updated_at TIMESTAMP,
-			PRIMARY KEY (id) )"""
+			PRIMARY KEY (id))"""
 		cursor.execute(sql)
 	connection.commit()
 finally:
--- crimemap/dbhelper.py
+++ crimemap/dbhelper.py
@@ -22,9 +22,9 @@ class DBHelper:
 	def add_input(self, data):
 		connection = self.connect()
 		try:
-			query = "INSERT INTO crime (description) VALUES('{}');".format(data)
+			query = "INSERT INTO crimes (description) VALUES('{}');".format(data)
 			with connection.cursor() as cursor:
-				cursor.exectue(query)
+				cursor.execute(query)
 				connection.commit()
 		finally:
 			connection.close()
