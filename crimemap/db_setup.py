a95d1ea83dd26af35f5054ded21c0476e4143feb
Abstract Thinking <abstract.thinking@w0lk37.de>
2016-06-25 16:53:51 +0000

    Fixed bugs and syntax

--- crimemap/db_setup.py
+++ crimemap/db_setup.py
@@ -3,20 +3,20 @@ import dbconfig
 
 connection = pymysql.connect(host='localhost',
 	user=dbconfig.db_user,
-	passed=dbconfig.db_password)
+	passwd=dbconfig.db_password)
 
 try:
 	with connection.cursor() as cursor:
 		sql = "CREATE DATABASE IF NOT EXISTS crimemap"
 		cursor.execute(sql)
 		sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
-			id int NOT NULL AUTO_INCREMENT,
+			id INT NOT NULL AUTO_INCREMENT,
 			latitude FLOAT(10,6),
 			longitude FLOAT(10,6),
 			date DATETIME,
 			category VARCHAR(1000),
 			updated_at TIMESTAMP,
-			PRIMARY KEY (id) """
+			PRIMARY KEY (id) )"""
 		cursor.execute(sql)
 	connection.commit()
 finally:
