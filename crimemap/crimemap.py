7bd7405c279381f6b589b6b507ac6b85abb26b27
Abstract Thinking <abstract.thinking@w0lk37.de>
2016-06-26 14:14:03 +0000

    add new crimes functionality

--- crimemap/crimemap.py
+++ crimemap/crimemap.py
@@ -3,6 +3,7 @@ if dbconfig.test:
 	from mockdbhelper import MockDBHelper as DBHelper
 else:
 	from dbhelper import DBHelper
+import json
 
 from flask import Flask
 from flask import render_template
@@ -13,12 +14,9 @@ DB = DBHelper()
 
 @app.route("/")
 def home():
-	try:
-		data = DB.get_all_inputs()
-	except Exception as e:
-		print e
-		data = None
-	return render_template("home.html", data=data)
+	crimes = DB_get_all_crimes()
+	crimes = json.dumps(crimes)
+	return render_template("home.html", crimes=crimes)
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -35,6 +33,16 @@ def clear():
 		DB.clear_all()
 	except Exception as e:
 		print e
+	return home()
+
+@app.route("/submitcrime")
+def submitcrime():
+	category = request.form.get("category")
+	date = request.form.get("date")
+	latitude = float(request.form.get("latitude"))
+	longitude= float(request.form.get("longitude"))
+	description = request.form.get("description")
+	DB.add_crime(category, date, latitude, longitude, description)
 	return home()
 
 if __name__ == '__main__':
