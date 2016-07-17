34bc322eda2540e1c88a09fdf3d22cd125fa5742
Abstract Thinking <abstract.thinking@w0lk37.de>
2016-07-17 19:02:03 +0000

    Bugfixing ^

--- waitercaller/mockdbhelper.py
+++ waitercaller/mockdbhelper.py
@@ -9,42 +9,42 @@ MOCK_REQUESTS = [{"_id" : "1", "table_number" : "1", "table_id" : "1", "time" : 
 
 class MockDBHelper:
 
-    def get_user(self, email):
-        user = [x for x in MOCK_USERS if x.get("email") == email]
-        if user:
-            return user[0]
-        return None
+	def get_user(self, email):
+		user = [x for x in MOCK_USERS if x.get("email") == email]
+		if user:
+			return user[0]
+        	return None
 
-    def add_user(self, email, salt, hashed):
-        MOCK_USERS.append({"email": email, "salt": salt, "hashed": hashed})
+	def add_user(self, email, salt, hashed):
+		MOCK_USERS.append({"email": email, "salt": salt, "hashed": hashed})
 
-    def add_table(self, number, owner):
-	MOCK_TABLES.append({"_id" : number, "number" : number, "owner" : owner})
+	def add_table(self, number, owner):
+		MOCK_TABLES.append({"_id" : number, "number" : number, "owner" : owner})
 
-    def update_table(self, _id, url):
-	for table in MOCK_TABLES:
-		if table.get("_id") == _id:
-			table["url"] = url
-			break
+	def update_table(self, _id, url):
+		for table in MOCK_TABLES:
+			if table.get("_id") == _id:
+				table["url"] = url
+				break
 
-    def get_tables(self, owner_id):
-	return MOCK_TABLES
+	def get_tables(self, owner_id):
+		return MOCK_TABLES
 
-    def delete_table(self, table_id):
-	for i, table in enumerate(MOCK_TABLES):
-		if table.get("_id") == table_id:
-			del MOCK_TABLES[i]
-			break
+	def delete_table(self, table_id):
+		for i, table in enumerate(MOCK_TABLES):
+			if table.get("_id") == table_id:
+				del MOCK_TABLES[i]
+				break
 
-    def add_table(self, number, owner):
- 	MOCK_TABLES.append("_id" : str(number), "number" : number, "owner" : owner)
-	return number
+	def add_table(self, number, owner):
+ 		MOCK_TABLES.append({"_id" : str(number), "number" : number, "owner" : owner})
+		return number
 
-    def get_requests(self, owner_id):
-	return MOCK_REQUESTS
+	def get_requests(self, owner_id):
+		return MOCK_REQUESTS
 
-    def delete_request(self, request_id):
-	for i, request[...]
-	    if requests [...]
-		del MOCK_REQUESTS[i]
-		break
+	def delete_request(self, request_id):
+		for i, request in enumerate(MOCK_REQUESTS):
+			if request.get("_id") == request_id:
+				del MOCK_REQUESTS[i]
+				break
