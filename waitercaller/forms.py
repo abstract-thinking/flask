2b72db2fffe21d6c7daab40c776f4a53fd0c23f2
Abstract Thinking <abstract.thinking@w0lk37.de>
2016-08-01 16:48:29 +0000

    Using WTForms

---
+++ waitercaller/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import Form
+from wtforms import PasswordField
+from wtforms import SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms import validators
+
+class RegisterForm(Form):
+	email = EmailField('email',
+		validators=[validators.DataRequired(), validators.Email()])
+	password = PasswordField('password', 
+		validators=[validators.DataRequired(), validators.Length(min=8,
+		message="Please choose a Password of at least 8 characters")])
+	password2 = PasswordField('password2',
+		validators=[validators.DataRequired(), validators.EqualTo('password',
+		message="Passwords must match")])
+	submit = SubmitField('submit', [validators.DataRequired()])
